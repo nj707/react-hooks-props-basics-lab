1d1c2914b96833514c8be59eaa2c25e18ef0c621
Nial Johnson <nialj707@gmail.com>
2023-08-22 12:31:17 -0700

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,18 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({ bio, About, linkedin, github }) {
+
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <h2>{About}</h2>
+      {!bio || bio === "" ? null : <p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
+      <Links
+        linkedin={linkedin}
+        github={github}
+      > </Links>
+    </div >
   );
 }
 
